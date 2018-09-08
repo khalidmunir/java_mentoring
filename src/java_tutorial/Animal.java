c6e021edecb67964c5accd0319c8e669287092bf
khalid_munir <khalid_munir@icloud.com>
2018-09-08 23:32:25 +0100

    first commit - set up repo

---
+++ src/java_tutorial/Animal.java
@@ -0,0 +1,23 @@
+package java_tutorial;
+
+
+import java.util.Scanner;
+import java.util.*; 
+
+public class Animal {
+
+	// Notes: 
+	// public <- obvious
+	// static <- shared by all objects 
+	// final <- is a constant, cannot be changed
+	// double <- the type of object (decimal number here)
+	public static final double FAVNUMBER = 1.6180;
+	
+	// Notes: 
+	// private <- only members of this class
+	// String <- the type of object (String here)
+	private String name;
+	
+	
+	
+}
---
+++ src/java_tutorial/Dog.java
@@ -0,0 +1,5 @@
+package java_tutorial;
+
+public class Dog {
+
+}
